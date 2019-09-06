6f645e142d7b1ceb5b080eb873240cdad70c5305
Melissa Christman <melchrstmn@gmail.com>
2019-09-06 19:09:15 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,11 +2,9 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       :patriarch => {}
-      
       :matriarch => {}
       :hero=> {}
       :hero_friends => []
-      
       }
       }
       }
